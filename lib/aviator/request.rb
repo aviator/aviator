42a79f3f13841b03b514d4388e584b9bae1e4c18
Mark Maglana <mmaglana@gmail.com>
2013-08-24 00:48:19 -0700

    Refactor Request such that some methods don't need class initialization

--- lib/aviator/request.rb
+++ lib/aviator/request.rb
@@ -1,104 +1,154 @@
 module Aviator
-  
+
   class Request
-    
+
     class ApiVersionNotDefinedError < StandardError
       def initialize
-        super "api_version is not defined in #{ self.class }"
+        super "api_version is not defined."
       end
     end
-    
+
     class EndpointTypeNotDefinedError < StandardError
       def initialize
-        super "endpoint_type is not defined in #{ self.class }"
+        super "endpoint_type is not defined."
       end
     end
 
     class PathNotDefinedError < StandardError
       def initialize
-        super "path is not defined in #{ self.class }"
+        super "path is not defined."
       end
     end
-    
-    
+
+
     def initialize(params={})
       validate_params(params)
       @params = params
-      
-      raise PathNotDefinedError.new unless respond_to?(:path)
-      raise EndpointTypeNotDefinedError.new unless respond_to?(:endpoint_type)
-      raise ApiVersionNotDefinedError.new unless respond_to?(:api_version)
     end
-    
-    
-    def allow_anonymous?
-      false
+
+
+    def anonymous?
+      self.class.anonymous?
     end
-    
-    
+
+
+    def api_version
+      self.class.api_version
+    end
+
+
     def body?
-      respond_to? :body
+      self.respond_to? :body
     end
-    
-    
+
+
+    def endpoint_type
+      self.class.endpoint_type
+    end
+
+
     def http_method
-      :get
+      self.class.http_method
     end
     
     
+    def path?
+      self.respond_to? :path
+    end
+
+
     def querystring?
       respond_to? :querystring
     end
-    
-    
+
+
     private
-    
+
+
     def params
       @params.dup
     end
-    
+
+
     def validate_params(params)
       validators = methods.select{ |name| name =~ /^param_validator_/ }
       validators.each do |name|
         send(name, params)
       end
     end
-    
-    
+
+
     class << self
+
+      def anonymous
+        # @anonymous will be defined by the descendant class
+        # where this method (or macro) is called.
+        @anonymous = true
+      end
+
+
+      def anonymous?
+        # @anonymous will be defined by the descendant class
+        @anonymous == true
+      end
+
+
+      def api_version(value=nil)
+        if value
+          # @api_version will be defined by the descendant
+          # class where this method is called.
+          @api_version = value
+        else
+          @api_version
+        end
+      end
+
+
+      def body?
+        instance_methods.include? :body
+      end
+
+
+      def endpoint_type(value=nil)
+        if value
+          # @endpoint_type will be defined by the descendant
+          # class where this method is called.
+          @endpoint_type = value
+        else
+          @endpoint_type
+        end
+      end
+
+
+      def http_method(value=nil)
+        if value
+          # @http_method will be defined by the descendant
+          # class where this method is called.
+          @http_method = value
+        else
+          @http_method
+        end
+      end
+
+
+      def path?
+        instance_methods.include? :path
+      end
       
+
       private
-      
-      def allow_anonymous
-        define_method :allow_anonymous?, lambda { true }
-      end
-      
-      
-      def api_version(value)
-        define_method :api_version, lambda { value }
-      end
-      
-      
-      def endpoint_type(value)
-        define_method :endpoint_type, lambda { value }
-      end
-      
-      
-      def http_method(http_method_name)
-        define_method :http_method, lambda { http_method_name }
-      end
-          
-      
+
+
       def requires_param(param_name)
         last_num = instance_methods.map{|n| n.to_s.gsub(/^param_validator_/, '').to_i }.max
-        
-        define_method "param_validator_#{ last_num + 1 }".to_sym, lambda { |params| 
+
+        define_method "param_validator_#{ last_num + 1 }".to_sym, lambda { |params|
           raise ArgumentError.new("Missing parameter #{ param_name }.") unless params.keys.include? param_name
         }
       end
-      
+
     end
-    
+
   end
-  
+
 end
--- test/aviator/request_test.rb
+++ test/aviator/request_test.rb
@@ -4,247 +4,246 @@ class Aviator::Test::Request < Aviator::Test::Base
 
   describe 'Aviator::Request' do
 
-    def valid_class
-      Class.new(Aviator::Request) do
-        endpoint_type :public
-        api_version :v2
-        def path; end
-      end
-    end
-
     describe '::new' do
-      
-      it 'raises an error if path is not defined' do
-        invalid_class = Class.new(valid_class) do
-          undef_method :path
-        end
-                      
-        initializer = lambda { invalid_class.new({}) }
-        initializer.must_raise Aviator::Request::PathNotDefinedError
-        
-        error = initializer.call rescue $!
-        
-        error.message.wont_be_nil
-      end
 
-      
       it 'raises an error when a required param is not provided' do
-        klass = Class.new(valid_class) do
-                  requires_param :name
+        klass = Class.new(Aviator::Request) do
+                  requires_param :someparamname
                 end
-                      
+      
         initializer = lambda { klass.new({}) }
         initializer.must_raise ArgumentError
-        
-        error = initializer.call rescue $!
-        
-        error.message.wont_be_nil
-      end
-  
-  
-      it 'does not raise any error when the required param is provided' do
-        klass = Class.new(valid_class) do
-                  requires_param :name
-                end
-                      
-        obj = klass.new({ name: 'somename' })
-      end
-
-
-      it 'raises an EndpointTypeNotDefinedError if endpoint type is not defined' do
-        invalid_class = Class.new(valid_class) do
-          undef_method :endpoint_type
-        end
-                      
-        initializer = lambda { invalid_class.new({}) }
-        initializer.must_raise Aviator::Request::EndpointTypeNotDefinedError
-        
-        error = initializer.call rescue $!
-        
-        error.message.wont_be_nil
-      end
-
-
-      it 'raises an ApiVersionNotDefinedError if endpoint type is not defined' do
-        invalid_class = Class.new(valid_class) do
-          undef_method :api_version
-        end
-                      
-        initializer = lambda { invalid_class.new({}) }
-        initializer.must_raise Aviator::Request::ApiVersionNotDefinedError
-        
-        error = initializer.call rescue $!
-        
-        error.message.wont_be_nil
-      end
-
       
+        error = initializer.call rescue $!
+      
+        error.message.wont_be_nil
+        error.message.must_include "someparamname"
+      end
+      
+      
+      it 'does not raise any error when the required param is provided' do
+        klass = Class.new(Aviator::Request) do
+                  requires_param :someparamname
+                end
+      
+        obj = klass.new({ someparamname: 'someparamvalue' })
+      end
+
     end
 
 
-    describe '#allow_anonymous?' do
+    describe '::anonymous?' do
 
       it 'is false by default' do
-        obj = valid_class.new
+        klass = Class.new(Aviator::Request)
 
-        obj.allow_anonymous?.must_equal false
+        klass.anonymous?.must_equal false
       end
 
 
       it 'returns true if specified as such' do
-        klass = Class.new(valid_class) do 
-                  allow_anonymous
+        klass = Class.new(Aviator::Request) do
+                  anonymous
                 end
-                
-        obj = klass.new
 
-        obj.allow_anonymous?.must_equal true
+        klass.anonymous?.must_equal true
       end
 
     end
 
 
-    describe '::allows_anonymous' do
+    describe '#anonymous?' do
 
-      it 'is a private class method' do
-        private_method = lambda { Aviator::Request.allow_anonymous }
-        private_method.must_raise NoMethodError
+      it 'is false by default' do
+        klass = Class.new(Aviator::Request)
 
-        error = private_method.call rescue $!
+        klass.new.anonymous?.must_equal false
+      end
 
-        error.message.wont_be_nil
+
+      it 'returns true if specified as such' do
+        klass = Class.new(Aviator::Request) do
+                  anonymous
+                end
+
+        klass.new.anonymous?.must_equal true
       end
 
     end
-    
-    
-    describe '#api_version' do
-      
-      it 'returns the api version as defined' do
-        klass = Class.new(valid_class) do
+
+
+    describe '::api_version' do
+
+      it 'returns the api version' do
+        klass = Class.new(Aviator::Request) do
           api_version :v2
         end
-        
+
+        klass.api_version.must_equal :v2
+      end
+
+    end
+
+
+    describe '#api_version' do
+
+      it 'returns the api version' do
+        klass = Class.new(Aviator::Request) do
+          api_version :v2
+        end
+
         klass.new.api_version.must_equal :v2
       end
-      
+
     end
-    
-    
-    describe '#body?' do
-      
+
+
+    describe '::body?' do
+
       it 'returns false if the body method is not defined' do
-        valid_class.new.body?.must_equal false
+        klass = Class.new(Aviator::Request)
+
+        klass.body?.must_equal false
       end
-      
+
 
       it 'returns true if the body method is defined' do
-        klass = Class.new(valid_class) do
+        klass = Class.new(Aviator::Request) do
           def body; end
         end
-        
-        klass.new.body?.must_equal true
+
+        klass.body?.must_equal true
       end
-      
+
     end
 
 
-    describe '#endpoint_type' do    
-      
-      it 'returns the endpoint type if it is defined' do
-        class_with_endpoint_type = Class.new(valid_class) do
-          endpoint_type :public
-        end
-        
-        class_with_endpoint_type.new.endpoint_type.must_equal :public
+    describe '#body?' do
+
+      it 'returns false if the body method is not defined' do
+        klass = Class.new(Aviator::Request)
+
+        klass.new.body?.must_equal false
       end
-      
+
+
+      it 'returns true if the body method is defined' do
+        klass = Class.new(Aviator::Request) do
+          def body; end
+        end
+
+        klass.new.body?.must_equal true
+      end
+
     end
 
 
     describe '::endpoint_type' do
 
-      it 'is a private class method' do
-        private_method = lambda { Aviator::Request.endpoint_type }
-        private_method.must_raise NoMethodError
+      it 'returns the endpoint type' do
+        klass = Class.new(Aviator::Request) do
+          endpoint_type :public
+        end
 
-        error = private_method.call rescue $!
-
-        error.message.wont_be_nil
+        klass.endpoint_type.must_equal :public
       end
 
     end
-        
-    
-    describe '#http_method' do
-      
-      it 'is set to get by default' do
-        obj = valid_class.new
-  
-        obj.http_method.must_equal :get
+
+
+    describe '#endpoint_type' do
+
+      it 'returns the endpoint type' do
+        klass = Class.new(Aviator::Request) do
+          endpoint_type :public
+        end
+
+        klass.new.endpoint_type.must_equal :public
       end
 
+    end
 
+
+    describe '::http_method' do
+    
       it 'returns the http method if it is defined' do
-        klass = Class.new(valid_class) do 
+        klass = Class.new(Aviator::Request) do
                   http_method :post
                 end
-                
-        obj = klass.new
-  
-        obj.http_method.must_equal :post
+          
+        klass.http_method.must_equal :post
       end
-      
+    
     end
     
     
-    describe '::http_method' do
-  
-      it 'is a private class method' do
-        private_method = lambda { Aviator::Request.http_method }
-        private_method.must_raise NoMethodError
-  
-        error = private_method.call rescue $!
-  
-        error.message.wont_be_nil
-        error.message.must_include "private method"
+    describe '#http_method' do
+    
+      it 'returns the http method if it is defined' do
+        klass = Class.new(Aviator::Request) do
+                  http_method :post
+                end
+          
+        klass.new.http_method.must_equal :post
       end
-  
+    
     end
-  
-  
-    describe '#querystring?' do
-      
-      it 'returns false if the querystring method is not defined' do
-        valid_class.new.querystring?.must_equal false
+    
+
+    describe '::path?' do
+
+      it 'returns false if the path method is not defined' do
+        klass = Class.new(Aviator::Request)
+
+        klass.path?.must_equal false
       end
-      
-      
-      it 'returns true if the querystring method is defined' do
-        klass = Class.new(valid_class) do
-          def querystring; end
+
+
+      it 'returns true if the path method is defined' do
+        klass = Class.new(Aviator::Request) do
+          def path; end
         end
-        
-        klass.new.querystring?.must_equal true
-      end
       
+        klass.path?.must_equal true
+      end
+
     end
-  
-    
+
+
+    describe '#path?' do
+
+      it 'returns false if the path method is not defined' do
+        klass = Class.new(Aviator::Request)
+
+        klass.new.path?.must_equal false
+      end
+
+
+      it 'returns true if the path method is defined' do
+        klass = Class.new(Aviator::Request) do
+          def path; end
+        end
+      
+        klass.new.path?.must_equal true
+      end
+
+    end
+
+
     describe '::requires_param' do
-  
+    
       it 'is a private class method' do
         private_method = lambda { Aviator::Request.requires_param }
         private_method.must_raise NoMethodError
-  
+    
         error = private_method.call rescue $!
-  
+    
         error.message.wont_be_nil
         error.message.must_include "private method"
       end
-  
+    
     end
-  
+
   end
 
 end
