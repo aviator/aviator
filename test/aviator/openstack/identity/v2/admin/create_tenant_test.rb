a70cba41f6db0fadb9f4fa91166a1b9e796d25aa
Mark Maglana <mmaglana@gmail.com>
2013-08-26 21:30:27 -0700

    Make create_tenant_test more succinct

--- test/aviator/openstack/identity/v2/admin/create_tenant_test.rb
+++ test/aviator/openstack/identity/v2/admin/create_tenant_test.rb
@@ -11,7 +11,7 @@ class Aviator::Test
         params[:enabled]     = true
       end
     end
-    
+
 
     def helper
       Aviator::Test::RequestHelper
@@ -25,94 +25,64 @@ class Aviator::Test
     end
 
 
-    describe '::endpoint_type' do
-      
-      it 'returns :admin' do
-        klass.endpoint_type.must_equal :admin
-      end
-      
+    it 'has the correct endpoint type' do
+      klass.endpoint_type.must_equal :admin
     end
 
 
-    describe '::api_version' do
-      
-      it 'returns :v2' do
+    it 'has the correct api version' do
         klass.api_version.must_equal :v2
-      end
-      
     end
-        
-    
-    describe '::http_method' do
-      
-      it 'returns :post' do
-        klass.http_method.must_equal :post
-      end
-      
-    end
-    
-    
-    describe '::required_params' do
-      
-      it 'returns the list of required params' do
-        klass.required_params.must_equal [:name, :description, :enabled]
-      end
-      
-    end
-    
-    
-    describe '#url' do
-      
-      it 'returns the correct value' do
-        session_data = helper.admin_session_data
-        service_spec = session_data[:access][:serviceCatalog].find{|s| s[:type] == 'identity' }
-        url = "#{ service_spec[:endpoints][0][:adminURL] }/tenants"
-        
-        request = create_request
-        
-        request.url.must_equal url
-      end
-      
-    end
-    
-    
-    describe '#headers' do
-      
-      it 'returns valid headers' do
-        headers = {
-          'X-Auth-Token' => helper.admin_session_data[:access][:token][:id]
-        }
-        
-        request = create_request
-        
-        request.headers.must_equal headers
-      end
-      
-    end
-    
-    
-    describe '#body' do
-      
-      it 'returns a valid body' do
-        params = {
-          name:        'Project',
-          description: 'My Project',
-          enabled:      true
-        }
-        
-        body = {
-          tenant: params
-        }
 
-        request = klass.new(helper.admin_session_data) do |p|
-          params.each do |k,v|
-            p[k] = params[k]
-          end
+
+    it 'has the correct http method' do
+      klass.http_method.must_equal :post
+    end
+
+
+    it 'has the correct list of required parameters' do
+      klass.required_params.must_equal [:name, :description, :enabled]
+    end
+
+
+    it 'has the correct url' do
+      session_data = helper.admin_session_data
+      service_spec = session_data[:access][:serviceCatalog].find{|s| s[:type] == 'identity' }
+      url = "#{ service_spec[:endpoints][0][:adminURL] }/tenants"
+
+      request = create_request
+
+      request.url.must_equal url
+    end
+
+
+    it 'has the correct headers' do
+      headers = { 'X-Auth-Token' => helper.admin_session_data[:access][:token][:id] }
+
+      request = create_request
+
+      request.headers.must_equal headers
+    end
+
+
+    it 'has the correct body' do
+      params = {
+        name:        'Project',
+        description: 'My Project',
+        enabled:      true
+      }
+
+      body = {
+        tenant: params
+      }
+
+      request = klass.new(helper.admin_session_data) do |p|
+        params.each do |k,v|
+          p[k] = params[k]
         end
-        
-        request.body.must_equal body
       end
-      
+
+      request.body.must_equal body
     end
 
   end
