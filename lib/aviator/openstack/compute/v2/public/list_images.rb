701ea92bb354e25f84b6044717fcef048329ea19
Mark Maglana <mmaglana@gmail.com>
2013-09-03 03:30:15 -0700

    Minor change to code structure

--- lib/aviator/openstack/compute/v2/public/list_images.rb
+++ lib/aviator/openstack/compute/v2/public/list_images.rb
@@ -1,54 +1,58 @@
-Aviator.define_request :list_images do
+module Aviator
+  
+  define_request :list_images do
 
-  meta :provider,      :openstack
-  meta :service,       :compute
-  meta :api_version,   :v2
-  meta :endpoint_type, :public
+    meta :provider,      :openstack
+    meta :service,       :compute
+    meta :api_version,   :v2
+    meta :endpoint_type, :public
 
-  link 'documentation',
-       'http://docs.openstack.org/api/openstack-compute/2/content/List_Images-d1e4435.html'
+    link 'documentation',
+         'http://docs.openstack.org/api/openstack-compute/2/content/List_Images-d1e4435.html'
 
-  param :details,        required: false
-  param :server,         required: false
-  param :name,           required: false
-  param :status,         required: false
-  param 'changes-since', required: false
-  param :marker,         required: false
-  param :limit,          required: false
-  param :type,           required: false
+    param :details,        required: false
+    param :server,         required: false
+    param :name,           required: false
+    param :status,         required: false
+    param 'changes-since', required: false
+    param :marker,         required: false
+    param :limit,          required: false
+    param :type,           required: false
 
 
-  def headers
-    h = {}
+    def headers
+      h = {}
 
-    unless self.anonymous?
-      h['X-Auth-Token'] = session_data[:access][:token][:id]
+      unless self.anonymous?
+        h['X-Auth-Token'] = session_data[:access][:token][:id]
+      end
+
+      h
     end
 
-    h
-  end
 
-
-  def http_method
-    :get
-  end
-
-
-  def url
-    service_spec = session_data[:access][:serviceCatalog].find{|s| s[:type] == 'compute' }
-
-    str  = "#{ service_spec[:endpoints][0][:publicURL] }/images"
-    str += "/detail" if params[:details]
-
-    filters = []
-
-    (optional_params + required_params - [:details]).each do |param_name|
-      filters << "#{ param_name }=#{ params[param_name] }" if params[param_name]
+    def http_method
+      :get
     end
 
-    str += "?#{ filters.join('&') }" unless filters.empty?
 
-    str
+    def url
+      service_spec = session_data[:access][:serviceCatalog].find{|s| s[:type] == 'compute' }
+
+      str  = "#{ service_spec[:endpoints][0][:publicURL] }/images"
+      str += "/detail" if params[:details]
+
+      filters = []
+
+      (optional_params + required_params - [:details]).each do |param_name|
+        filters << "#{ param_name }=#{ params[param_name] }" if params[param_name]
+      end
+
+      str += "?#{ filters.join('&') }" unless filters.empty?
+
+      str
+    end
+
   end
 
 end
