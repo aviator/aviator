2491d4fb83ad8a7ec99ee6b549d201f2c5457f5a
Mark Maglana <mmaglana@gmail.com>
2013-09-10 01:35:24 -0700

    Implements #13: delete_image

---
+++ lib/aviator/openstack/compute/v2/public/delete_image.rb
@@ -0,0 +1,40 @@
+module Aviator
+  
+  define_request :delete_image do
+
+    meta :provider,      :openstack
+    meta :service,       :compute
+    meta :api_version,   :v2
+    meta :endpoint_type, :public
+
+    link 'documentation',
+         'http://docs.openstack.org/api/openstack-compute/2/content/Delete_Image-d1e4957.html'
+
+    param :id, required: true
+
+
+    def headers
+      h = {}
+
+      unless self.anonymous?
+        h['X-Auth-Token'] = session_data[:access][:token][:id]
+      end
+
+      h
+    end
+
+
+    def http_method
+      :delete
+    end
+
+
+    def url
+      service_spec = session_data[:access][:serviceCatalog].find{|s| s[:type] == service.to_s }
+
+      "#{ service_spec[:endpoints][0][:publicURL] }/images/#{ params[:id]}"
+    end
+
+  end
+
+end
