bbe057a22eeba9e45a888de8391e2870201ee05e
Alfonso Juan Dillera <aj.dillera@gmail.com>
2013-09-24 11:52:12 +0800

    [WIP] Added query for flavor details.

---
+++ lib/aviator/openstack/compute/v2/public/get_flavor_details.rb
@@ -0,0 +1,29 @@
+module Aviator
+
+  define_request :get_flavor_details, inherit: [:openstack, :common, :v2, :public, :base] do
+
+    meta :service, :compute
+
+    link 'documentation',
+         'http://docs.openstack.org/api/openstack-compute/2/content/Get_Flavor_Details-d1e4317.html'
+
+    param :id, required: true
+
+
+    def headers
+      super
+    end
+
+
+    def http_method
+      :get
+    end
+
+
+    def url
+      "#{ base_url_for :public }/flavors/#{ params[:id] }"
+    end
+
+  end
+
+end
