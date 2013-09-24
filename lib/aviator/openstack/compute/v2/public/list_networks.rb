d5dad674ec786175a74a7364b798e244ce00ef20
Alvin Garcia <agarcia1337@gmail.com>
2013-09-24 13:19:46 +0800

    [compute] Add list_networks request

---
+++ lib/aviator/openstack/compute/v2/public/list_networks.rb
@@ -0,0 +1,27 @@
+module Aviator
+
+  define_request :list_networks, inherit: [:openstack, :common, :v2, :public, :base] do
+
+    meta :service, :compute
+
+    link 'documentation',
+         'http://api.openstack.org/api-ref-compute.html#ext-os-networks'
+
+
+    def headers
+      super
+    end
+
+
+    def http_method
+      :get
+    end
+
+
+    def url
+      "#{ base_url_for :public }/os-networks"
+    end
+
+  end
+
+end
