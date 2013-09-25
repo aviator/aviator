bfadb69df18d31be7b3a5314c522d53c33ddcb02
Alvin Garcia <agarcia1337@gmail.com>
2013-09-25 12:39:36 +0800

    [compute] Add reset_server request

---
+++ lib/aviator/openstack/compute/v2/admin/reset_server.rb
@@ -0,0 +1,39 @@
+module Aviator
+
+  define_request :reset_server, inherit: [:openstack, :common, :v2, :admin, :base] do
+
+    meta :service, :compute
+
+    link 'documentation',
+         'http://docs.openstack.org/api/openstack-compute/2/content/POST_os-resetState_v2__tenant_id__servers__server_id__action_ext-os-admin-actions.html'
+
+    param :id,    required: true
+    param :state, required: true
+
+
+    def body
+      {
+        'os-resetState' => {
+          'state' => params[:state]
+        }
+      }
+    end
+
+
+    def headers
+      super
+    end
+
+
+    def http_method
+      :post
+    end
+
+
+    def url
+      "#{ base_url_for :admin }/servers/#{ params[:id] }/action"
+    end
+
+  end
+
+end
