54dc07e7edcb9fbc0fe78c1d4263ca8f11c5b9a6
Alvin Garcia <agarcia1337@gmail.com>
2013-10-08 13:30:20 +0800

    [compute] Add delete_image_metadata_item request

---
+++ lib/aviator/openstack/compute/v2/public/delete_image_metadata_item.rb
@@ -0,0 +1,31 @@
+module Aviator
+
+  define_request :delete_image_metadata_item, inherit: [:openstack, :common, :v2, :public, :base] do
+
+    meta :service, :compute
+
+    link 'documentation',
+         'http://docs.openstack.org/api/openstack-compute/2/content/Delete_Metadata_Item-d1e5790.html'
+
+
+    param :id,  required: true
+    param :key, required: true
+
+
+    def headers
+      super
+    end
+
+
+    def http_method
+      :delete
+    end
+
+
+    def url
+      "#{ base_url }/images/#{ params[:id] }/metadata/#{ params[:key] }"
+    end
+
+  end
+
+end
