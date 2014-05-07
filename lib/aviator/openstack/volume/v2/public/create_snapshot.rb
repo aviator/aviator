691391b34b73c06fc466838dc268ac1439017ee9
Joe Ocaba <jocaba@exist.com>
2014-05-07 18:02:19 +0800

    [AVIATORAPI-56] moving create volume snapshot to v2

--- lib/aviator/openstack/volume/v2/public/create_snapshot.rb
+++ lib/aviator/openstack/volume/v2/public/create_snapshot.rb
@@ -3,31 +3,26 @@ module Aviator
   define_request :create_snapshot, inherit: [:openstack, :common, :v2, :public, :base] do
     
     meta :service,        :volume
-    meta :api_version,    :v1
+    meta :api_version,    :v2
 
     link 'documentation', 'http://docs.rackspace.com/cbs/api/v1.0/cbs-devguide/content/POST_createSnapshot_v1__tenant_id__snapshots_snapshots.html'
 
-    param :snapshot,            required: true
+    #param :snapshot,            required: true
     param :volume_id,           required: true
     param :force,               required: false
     param :display_name,        required: true
     param :display_description, required: true
     
     def body
-      p = {
+      {
         snapshot: {
          display_name:          params[:display_name],
          display_description:   params[:display_description],
          volume_id:             params[:volume_id],
-         force:                 params[:force]
+         force:                 params[:force] || false
         }
       }
-
-      optional_params.each do |key|
-          p[:volume][key] = params[key] if params[key]
-      end
-
-      p
+      
     end
 
     def headers
@@ -39,7 +34,9 @@ module Aviator
     end
 
     def url
-      "#{ base_url }/volumes/#{ params[:id] }/snapshots"
+      service_spec = session_data[:access][:serviceCatalog].find{|s| s[:type] == 'volumev2' }
+      v2_url = service_spec[:endpoints][0][:adminURL]
+      "#{ v2_url }/snapshots"
     end
   end
 
