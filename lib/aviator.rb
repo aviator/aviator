eae591eee913ae706910095a3f167d175a258012
Mark Maglana <mmaglana@gmail.com>
2014-06-03 11:34:18 -0700

    Add lib path to load path if it's not already there
    Change-Id: I78430112a63a97c665ef0a50771b61c4a10e76da

--- lib/aviator.rb
+++ lib/aviator.rb
@@ -1,1 +1,6 @@
+# Add the parent dir to the load path. This is for when
+# Aviator is not installed as a gem
+lib_path = File.dirname(__FILE__)
+$LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include? lib_path
+
 require 'aviator/core'
