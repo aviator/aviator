a148ebeb8319d873ad744dc3eea95f300d1517f8
Stefany Serino <sserino@exist.com>
2014-07-10 10:12:42 +0800

    [volume] add all_tenants parameter to list_snapshots

--- lib/aviator/openstack/volume/v2/public/list_snapshots.rb
+++ lib/aviator/openstack/volume/v2/public/list_snapshots.rb
@@ -9,7 +9,8 @@ module Aviator
 
     link 'documentation', 'http://docs.openstack.org/api/openstack-block-storage/2.0/content/GET_getSnapshotsDetail__v2__tenant_id__snapshots_detail_Snapshots.html'
 
-    param :detail, required: false
+    param :all_tenants,    required: false
+    param :detail,         required: false
 
     def headers
       super
@@ -22,10 +23,21 @@ module Aviator
     def url
       service_spec = session_data[:catalog].find{|s| s[:type] == 'volumev2' }
       v2_url = service_spec[:endpoints].find{|e| e[:interface] == 'admin'}[:url]
+
       str = "#{ v2_url }/snapshots"
       str += "/detail" if params[:detail]
+
+      filters = []
+
+      (optional_params + required_params - [:detail]).each do |param_name|
+        value = param_name == :all_tenants && params[param_name] ? 1 : params[param_name]
+        filters << "#{ param_name }=#{ value }" if value
+      end
+
+      str += "?#{ filters.join('&') }" unless filters.empty?
       str
     end
+
   end
 
 end
