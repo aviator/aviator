5e3ea898c3647244a0abd5ee7f0c2fd6960c88fa
Leo Martin Villasica <lvillasica@lvillasica-Vostro-3550.(none)>
2014-10-29 15:46:28 +0800

    Update Network

---
+++ lib/aviator/openstack/compute/v2/public/update_network.rb
@@ -0,0 +1,49 @@
+module Aviator
+
+  define_request :update_network, inherit: [:openstack, :common, :v2, :public, :base] do
+
+    meta :service, :compute
+
+    link 'documentation',
+         'http://api.openstack.org/api-ref-compute.html#ext-os-networks'
+
+
+    param :id,                   required: true
+    param :associate_host,       required: false
+    param :disassociate,         required: false
+    param :disassociate_project, required: false
+    param :disassociate_host,    required: false
+
+
+    def body
+      p = {}
+
+      optional_params = [
+        :associate_host, :disassociate_project,
+        :disassociate_project, :disassociate_host
+      ]
+
+      optional_params.each do |key|
+        p[key] = params[key] if params[key]
+      end
+
+      p
+    end
+
+    def headers
+      super
+    end
+
+
+    def http_method
+      :post
+    end
+
+
+    def url
+      "#{ base_url }/os-networks/#{ params[:id] }/action"
+    end
+
+  end
+
+end
